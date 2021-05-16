ef01f6284110996b006a6d035e92ae853f488a04
平野裕也 <hiranoyuya@hiranoyuuyanoMacBook-Air.local>
2021-05-16 21:46:25 +0900

    book_sumカラムをBookテーブルに追加

--- back/db/schema.rb
+++ back/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_11_115940) do
+ActiveRecord::Schema.define(version: 2021_05_16_123216) do
 
   create_table "active_storage_attachments", charset: "utf8mb4", force: :cascade do |t|
     t.string "name", null: false
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2021_04_11_115940) do
     t.string "author"
     t.string "title"
     t.string "image"
+    t.text "book_sum"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
